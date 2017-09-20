012360d4177939033d933ff0dc40dc5496eafede
duyhong <duyhong@berkeley.edu>
2017-09-20 15:03:46 -0700

    address-bloc_checkpoint7_asssignment: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -65,4 +65,13 @@ class AddressBook
      # #5
      return nil
    end
+
+   def iterative_search(name)
+     entries.each do |entry|
+       if name == entry.name
+         return entry
+       end
+     end
+     return nil
+   end
 end
